e3b4e1a6f8b2824eaeac3722f193f356ccaf4909
Sergei Kononov <sergei.kononov@gmail.com>
2010-02-26 13:51:49 +0300

    api and anget referencables added

--- src/nodeset/agent/__init__.py
+++ src/nodeset/agent/__init__.py
@@ -8,11 +8,17 @@ from twisted.application import service
 from proxy import ApiProxy
 from nodeset.common.twistedapi import run
 
+class AgentInstance(object):
+    
+    def __init__(self, config):
+        pass
+
 def run_agent():
     tub = Tub()
     tub.listenOn('tcp:5333')
     tub.setLocation('localhost:5333')
-    tub.registerReference(ApiProxy(), 'agent')
+    tub.registerReference(ApiProxy(), 'api')
+    tub.registerReference(AgentInstance, 'agent')
     
     application = service.Application("nodeset-agent")
     tub.setServiceParent(application)
