ead79bfa896e09c00f33d4ad7c2afe433590bb71
Sergei Kononov <sergei.kononov@gmail.com>
2010-03-11 18:07:46 +0300

    inline documentation added

--- src/nodeset/core/heartbeat.py
+++ src/nodeset/core/heartbeat.py
@@ -1,6 +1,9 @@
 from twisted.internet import reactor, defer
 
 class NodeMonitor:
+    """
+    Special class for handling heartbeat monitor and its callbacks and errbacks
+    """
     
     def __init__(self, node):
         self.node = node
@@ -17,11 +20,23 @@ class NodeMonitor:
             d.addErrback(callable, *args, **kwargs)
             
     def onOk(self, callable, *args, **kwargs):
+        """
+        @param callable: callable instance which will be added addCallback for callRemote defer
+        @param *args: additional arguments
+        @param **kwargs: additional kw arguments
+        @return: L{NodeMonitor}
+        """
         self.__ok.append((callable, args, kwargs))
     
         return self
     
     def onFail(self, callable, *args, **kwargs):
+        """
+        @param callable: callable instance which will be added by addErrback for callRemote defer
+        @param *args: additional args for errback
+        @param **kwargs: additional kw args for errback
+        @return: L{NodeMonitor}
+        """
         self.__fails.append((callable, args, kwargs))
         
         return self
@@ -34,12 +49,25 @@ class NodeHeartBeat:
         self.dispatcher = dispatcher
         
     def _lookup(self, node):
+        """
+        Do lookup for heartbeat monitor for node
+        @param node: Node
+        @type node: L{Node}
+        @raise: KeyError
+        @return: L{NodeMonitor}
+        """
         for m in self.monitors:
             if m.node == node:
                 return m
         raise KeyError("Unknown node %s" % node)
 
     def has(self, node):
+        """
+        Check if node has heartbeat monitor
+        @param node: Node
+        @type node: L{Node}
+        @return: True or False
+        """
         try:
             m = self._lookup(node)
             return True
@@ -47,24 +75,39 @@ class NodeHeartBeat:
             return False
     
     def add(self, node):
+        """
+        Add heartbeat monitor for node
+        @param node: Node
+        @type node: L{Node}
+        @return: L{NodeMonitor}
+        """
         monitor = NodeMonitor(node)
         self.monitors.add(monitor)
         
         return monitor
     
     def remove(self, node):
+        """
+        Removes heartbeat monitor for node
+        @param node: Node
+        @type node: L{Node}
+        @return: L{NodeMonitor}
+        """
         monitor = self._lookup(node) 
         self.monitors.remove(monitor)
         
         return monitor
     
     def schedule(self, delay=5):
+        """
+        Schedules heartbeating
+        """
         reactor.callLater(delay, self._do_heartbeat)
         
     def _do_heartbeat(self):
-        print self.monitors
-        print self.dispatcher.routing.entries
-        
+        """
+        Do actual heartbeat
+        """
         # copy monitors list, set can be changed through iteration
         monitors = self.monitors.copy()
         
@@ -72,6 +115,7 @@ class NodeHeartBeat:
             m.heartbeat()
             
         del monitors
+        
         self.schedule()
 
             
